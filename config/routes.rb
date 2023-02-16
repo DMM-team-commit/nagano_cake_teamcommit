4fdde4393dcc25ea8bb92d4d65126f45d1f2f758
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-02-16 09:03:37 +0000

    secondcommit

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,29 @@
 Rails.application.routes.draw do
+  
+   devise_for :customers,skip: [:passwords], controllers: {
+   registrations: "public/registrations",
+ sessions: 'public/sessions'
+ }
+  
+ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+ sessions: "admin/sessions"
+ }
   namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
+    get 'top' => 'homes#top', as: 'top'
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
+  
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about', as: 'about'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:index, :show, :edit, :update, :destroy]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :orders, only: [:new, :show, :create, :index]
   end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :admin do
-    get 'sessions/new'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/show'
-    get 'orders/index'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'sessions/new'
-  end
-  devise_for :admins
-  devise_for :customers
+  
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
