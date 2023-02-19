64d651e29c502d668fbdd799c293671e5e8cf7c7
EC2 Default User <ec2-user@ip-172-31-8-232.ap-northeast-1.compute.internal>
2023-02-19 15:11:45 +0000

    Addadmincontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,9 @@
 Rails.application.routes.draw do
-  
-  namespace :public do
-    get 'delivery_addresses/index'
-    get 'delivery_addresses/edit'
-  end
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-   devise_for :customers,skip: [:passwords], controllers: {
-   registrations: "public/registrations",
- sessions: 'public/sessions'
- }
-  
- devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
- sessions: "admin/sessions"
- }
-  namespace :admin do
-    get 'top' => 'homes#top', as: 'top'
-    resources :items, only: [:index, :new, :create, :edit, :show, :update]
-    resources :genres, only: [:index, :create, :edit, :update]
-    resources :customers, only: [:index, :show, :edit, :update]
-    resources :orders, only: [:show, :update]
-    resources :order_details, only: [:update]
-  end
+  #顧客用
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   
   scope module: :public do
     root to: 'homes#top'
@@ -45,8 +20,33 @@ Rails.application.routes.draw do
     resources :orders, only: [:new, :show, :create, :index]
     post '/orders/confirm' => 'orders#confirm'
     get '/orders/complete' => 'customers#complete'
+    #get 'delivery_addresses/index'
+    #get 'delivery_addresses/edit'
     resources :delivery_addresses, only: [:index, :edit, :create, :update, :destroy]
   end
+  
+  
+  #管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  namespace :admin do
+    #get 'orders/show'
+    #get 'items/index'
+    #get 'items/new'
+    #get 'items/show'
+    #get 'items/edit'
+    get 'top' => 'homes#top', as: 'top'
+    resources :items, only: [:index, :new, :create, :edit, :show, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+    get "orders/customers/:id" => "orders#index"
+  end
+  
+  
   
   
 
