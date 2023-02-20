a3216e4bf17c8c6274f65a574d7291c7768bcbb1
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-02-20 06:03:48 +0000

    [Start]mvc_delivery_address

--- app/controllers/public/delivery_addresses_controller.rb
+++ app/controllers/public/delivery_addresses_controller.rb
@@ -1,14 +1,44 @@
 class Public::DeliveryAddressesController < ApplicationController
+  before_action :authenticate_customer!
+  
   def index
+    @delivery_address = DeliveryAddress.new
+    @delivery_address.customer_id = current_customer.id
+    
   end
 
   def edit
+    @delivery_address = DeliveryAddress.find(params[:id])
   end
   
   def create
+    @delivery_address = DeliveryAddress.new(delivery_address_params)
+    @delivery_address.customer_id = current_customer.id
+    if  @delivery_address.save
+     redirect_to customers_delivery_address_path
+    else
+     @delivery_address = DeliveryAddress.new
+     @delivery_address.customer_id = current_customer.id
+     render 'index'
+    end
+  end
+  
+  def update
+    delivery_address = DeliveryAddress.find(params[:id])
+    delivery_address.update(delivery_address_params)
+    redirect_to customers_delivery_address_path
   end
 
   def destroy
+    delivery_address = DeliveryAddress.find(params[:id])
+    delivery_address.destroy
+    redirect_to customers_delivery_address_path
+  end
+  
+    private
+
+  def delivery_address_params
+    params.require(:delivery_address).permit(:customer_id, :post_code, :address, :addressee)
   end
   
 end
--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -1,2 +1,5 @@
 class DeliveryAddress < ApplicationRecord
+    belongs_to :customer
+    validate :address, presence :true
+    validate :addressee, presence :true
 end
