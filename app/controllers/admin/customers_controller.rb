10cd30ee6b85ba114a0f3827ee5351106457fd88
rcs04aaa <122098552+rcs04aaa@users.noreply.github.com>
2023-02-20 05:51:39 +0000

    [update]customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,8 +1,10 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = current_customer
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   namespace :public do
     get 'delivery_addresses/index'
     get 'delivery_addresses/edit'
