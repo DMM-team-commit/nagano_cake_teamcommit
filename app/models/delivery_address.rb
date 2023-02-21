dece4fbdd53f49c69e842b566acfd99b209685ad
EC2 Default User <ec2-user@ip-172-31-44-91.ap-northeast-1.compute.internal>
2023-02-21 05:19:52 +0000

    [fin?]address

--- app/models/delivery_address.rb
+++
@@ -1,6 +0,0 @@
-class DeliveryAddress < ApplicationRecord
-    belongs_to :customer
-    validates :post_code, length: { is: 7 }, numericality: { only_integer: true }
-    validates :address, presence: true
-    validates :addressee, presence: true
-end
