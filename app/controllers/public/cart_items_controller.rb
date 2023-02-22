d924177bfb755893d8bd1fc38784aa726cf1f98e
EC2 Default User <ec2-user@ip-172-31-37-17.ap-northeast-1.compute.internal>
2023-02-22 08:06:40 +0000

    cart still errors

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -14,9 +14,9 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-      cart_item.item_amount += params[:cart_item][:item_amount].to_i
+    if current_customer.cart_items.find_by(item_id: params[:item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:item][:item_id])
+      cart_item.amount += params[:item][:amount].to_i
       cart_item.save
     else
       @cart_item.save
@@ -24,6 +24,7 @@ class Public::CartItemsController < ApplicationController
     @cart_items = CartItem.all
     redirect_to cart_items_path
   end
+
 
   def index
     @cart_items = CartItem.where(customer_id: current_customer.id)
@@ -33,7 +34,7 @@ class Public::CartItemsController < ApplicationController
 
   def update
     cart_item = CartItem.find(params[:id])
-    cart_item.update(cart_item_params)
+    cart_item.update(item_params)
     @cart_items = CartItem.all
     redirect_to cart_items_path
   end
@@ -42,7 +43,7 @@ class Public::CartItemsController < ApplicationController
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:customer_id, :item_id, :item_amount)
+    params.require(:cart_item).permit(:customer_id, :item_id, :amount)
   end
 
 end
