5eefa72bdfacb236984b27c8fa18a64980698920
EC2 Default User <ec2-user@ip-172-31-37-17.ap-northeast-1.compute.internal>
2023-02-23 02:41:51 +0000

    order detail model fin

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,3 +1,10 @@
 class OrderDetail < ApplicationRecord
-    belongs_to :order
+  belongs_to :order
+  belongs_to :item
+
+  enum making_status: { cannnot_make: 0, waiting_make: 1, in_making: 2, make_complete: 3 }
+
+  def subtotal
+    price*amount
+  end
 end
